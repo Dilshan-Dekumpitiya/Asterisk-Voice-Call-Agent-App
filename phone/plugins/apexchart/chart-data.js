d89e16701c53b185b1524d2edcfb4bc1c6abcbf8
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2024-07-11 20:14:32 +0530

    update dashboard chart

--- phone/plugins/apexchart/chart-data.js
+++ phone/plugins/apexchart/chart-data.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
                 tooltip: {
                     y: {
                         formatter: function(val) {
-                            return "$ " + val + " thousands"
+                            return val + " Calls"
                         }
                     }
                 }
